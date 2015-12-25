8df166771ac7b1c557aeb66af098c1f2ce7f0e85
ranger <cngufan@gmail.com>
2015-12-25 11:58:56 +0800

    script update

--- fileProcess.py
+++ fileProcess.py
@@ -2,9 +2,10 @@
 # -*- coding = utf - 8 -*-
 
 import re
+import os
 
 class file_process:
-    def config_filter(self):
+    def configFilter(self):
         fin1 = open("refer.txt", 'r')
         fout1 = open("step1.txt", 'a')
         
@@ -25,23 +26,66 @@ class file_process:
         fin1.close()
         fout1.close()
 
-	def choose(self):
-		fin2 = open("step1.txt", 'r')
+    def lineNumChoose(self):
+        fin2 = open("step1.txt", 'r')
         fout2 = open("step2.txt", 'a')
 
+        n = -1
         while True:
+            n += 1
             line = fin2.readline()
             if line:
-                chooseline = line.split(":")[0]
-                fout2.write(chooseline)
+                if n % 5 == 0:
+                    chooseline = line.split(",")[0]
+                    fout2.write(chooseline + '\n')
             else:
                 break
 
         fin2.close()
         fout2.close()
 
+    def configExtract(self):
+        fin3 = open("step2.txt", 'r')
+        fin4 = open("remain.txt", 'r')
+        fout3 = open("part2.txt", 'a')
+
+        for remainLine in fin4.readlines():
+            remainLineBack = remainLine
+            remainLineNum = remainLine.split(":")[0]
+
+            fin3.seek(0)
+            for chooseLine in fin3.readlines():
+                line = chooseLine.strip()
+
+                if line == remainLineNum:
+                    fout3.write(remainLineBack)
+
+        fin3.close()
+        fin4.close()
+        fout3.close()
+        
+        os.remove("step1.txt")
+        os.remove("step2.txt")
+
+    def remainUpdate(self):
+        fout4 = open("remain_new.txt", 'a')
+        fin5 = open("remain.txt", 'r')
+        fin6 = open("part2.txt", 'r')
+
+        content = fin5.read()
+        for replaceLine in fin6.readlines():
+            print replaceLine
+            content.replace(replaceLine, "")
+
+        fout4.write(content)
+        fout4.close()
+        fin5.close()
+        fin6.close()
+
 
 if __name__ == "__main__":
     fp = file_process()
-    fp.config_filter()
-    #fp.choose()
+#    fp.configFilter()
+#    fp.lineNumChoose()
+    fp.configExtract()
+#    fp.remainUpdate()
