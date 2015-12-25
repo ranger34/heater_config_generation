f45a042f6d9d7090f548c430e79c7315cfae166c
ranger <cngufan@gmail.com>
2015-12-25 22:18:21 +0800

    close to finish

--- fileProcess.py
+++ fileProcess.py
@@ -3,6 +3,7 @@
 
 import re
 import os
+import random
 
 class file_process:
     def configFilter(self):
@@ -72,20 +73,83 @@ class file_process:
         fin5 = open("remain.txt", 'r')
         fin6 = open("part2.txt", 'r')
 
-        content = fin5.read()
-        for replaceLine in fin6.readlines():
-            print replaceLine
-            content.replace(replaceLine, "")
+        lineArray = []
+        for remainLine in fin5.readlines():
+			lineArray.append(remainLine.replace("\n", ""))
 
-        fout4.write(content)
+        for line in lineArray:
+            fin6.seek(0)
+            for replaceLine in fin6.readlines():
+                replaceLine = replaceLine.strip()
+                if line == replaceLine:
+					lineArray.remove(line)
+            fout4.write(line + '\n')
+
         fout4.close()
         fin5.close()
         fin6.close()
 
+    def combComplete(self):
+		fin7 = open("remain_new.txt", 'r')
+		fin8 = open("part1.txt", 'r')
+		fin9 = open("part2.txt", 'r')
+		fout5 = open("sample.txt", 'a')
+
+		for line1 in fin8.readlines():
+			fout5.write(line1.split(":")[1])
+
+		for line2 in fin9.readlines():
+			fout5.write(line2.split(":")[1])
+
+		lineArray = []
+		for line3 in fin7.readlines():
+			lineArray.append(line3.split(":")[1])
+		randStrArray = random.sample(lineArray, 62)
+		
+		for line4 in randStrArray:
+			fout5.write(line4)
+
+		fin7.close()
+		fin8.close()
+		fin9.close()
+		fout5.close()
+
+		#os.remove("part1.txt")
+		#os.remove("part2.txt")
+
+    def sampleGen(self):
+		fin10 = open("sample.txt", 'r')
+		fout6 = open("powerSample.txt", 'a')
+		fout7 = open("tempLowerSample.txt", 'a')
+		fout8 = open("tempUpperSample.txt", 'a')
+
+		for line in fin10.readlines():
+			p_str = "/*modify here*/{" + (line.split(",")[0])[1:] + "," + line.split(",")[1] + "}, {"\
+					+ (line.split(",")[4])[1:] + "," + line.split(",")[5] + "}, {"\
+					+ (line.split(",")[8])[1:] + "," + line.split(",")[9] + "}, {"\
+					+ (line.split(",")[12])[1:] + "," + line.split(",")[13] + "}, {"\
+					+ (line.split(",")[16])[1:] + "," + line.split(",")[17] + "}/*end modify*/"
+			tl_str = "/*modify here*/double temp_lower[rid_t] = {" + (line.split(",")[2])[1:] + ".0,"\
+					+ line.split(",")[6] + ".0," + line.split(",")[10] + ".0,"\
+					+ line.split(",")[14] + ".0," + line.split(",")[18] + ".0}/*end modify*/"
+			th_str = "/*modify here*/double temp_upper[rid_t] = {" + (line.split(",")[3])[1:] + ".0,"\
+					+ line.split(",")[7] + ".0," + line.split(",")[11] + ".0,"\
+					+ line.split(",")[15] + ".0, " + line.split(",")[19].strip() + ".0}/*end modify*/"
+
+			fout6.write(p_str + '\n')
+			fout7.write(tl_str + '\n')
+			fout8.write(th_str + '\n')
+
+		fin10.close()
+		fout6.close()
+		fout7.close()
+		fout8.close()
 
 if __name__ == "__main__":
     fp = file_process()
 #    fp.configFilter()
 #    fp.lineNumChoose()
-    fp.configExtract()
+#    fp.configExtract()
 #    fp.remainUpdate()
+#    fp.combComplete()
+    fp.sampleGen()
